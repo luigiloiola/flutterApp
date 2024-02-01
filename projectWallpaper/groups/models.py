6dc8b0b6ed4f5f765b8a1556d6c6ccbb82165b8d
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-01 18:05:06 -0300

    allowed hosts

--- projectWallpaper/groups/models.py
+++
@@ -1,7 +0,0 @@
-# groups/models.py
-from django.db import models
-from django.contrib.auth.models import User
-
-class Group(models.Model):
-    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Adicione outros campos conforme necessário
--- projectWallpaper/groups/views.py
+++
@@ -1,8 +0,0 @@
-# groups/views.py
-from rest_framework import viewsets
-from .models import Group
-from .serializers import GroupSerializer
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
